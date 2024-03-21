2f6c87a9aac6871f10b052279b4af140b56ed62e
Febrio Evan Hartanto <91063064+FebrioEvanHartanto@users.noreply.github.com>
2024-03-22 06:40:49 +0700

    Feature/unit testing (#2)
    * Unit Testing
    
    * Unit Testing
    
    * fixed config.js test and dev host

--- config/config.js
+++ config/config.js
@@ -4,14 +4,14 @@ const config = {
       username  : process.env.POSTGRES_USER,
       password  : process.env.POSTGRES_PASSWORD,
       database  : process.env.POSTGRES_DB,
-      host      : process.env.DB_HOST,
+      host      : "localhost",
       dialect   : "postgres"
   },
     test : {
       username  : process.env.POSTGRES_USER,
       password  : process.env.POSTGRES_PASSWORD,
       database  : process.env.DB_TEST,
-      host      : process.env.DB_HOST,
+      host      : "localhost",
       dialect   : "postgres"
   },
     docker : {
